f74042c10d38114230f4a295c08355221b59c6db
divijendra <divijendrasampth4054@gmail.com>
2021-12-13 22:39:21 +0530

    Refactored functions and classes

---
+++ classes.py
@@ -0,0 +1,39 @@
+import math
+
+class Billet:
+    def __init__(self):
+        self.density = float(input("Enter value of density (tons/m3): "))
+        self.width = float(input("Enter width of the billet (mm): ")) / 1000
+        self.thickness = float(input("Enter thickness of the billet (mm): ")) / 1000
+        self.length = float(input("Enter length of billet (mm): ")) / 1000
+        self.volume = self.width * self.length * self.thickness
+
+
+class Product:
+    def __init__(self, billet):
+        self.diameter = float(input("Enter diameter of the finished product (mm): ")) / 1000
+        self.max_speed = float(input("Enter maximum finishing speed (m/sec): "))
+        self.min_speed = float(input("Enter minimum finishing speed (m/sec): "))
+        self.max_prod_rate = float(input("Enter maximum production rate (tons/hr): "))
+        self.wt_frac = float(
+            input("Enter required weight in terms of percentage of nominal weight required (%): ")) / 100
+        self.vol_frac = 1 - float(input("Enter percentage of product lost during processing (%): ")) / 100
+        self.min_time_gap = float(input("Enter minimum time gap at finishing stage (seconds): "))
+        self.utilisation = float(input("Enter utilisation (%): ")) / 100
+        self.demand = float(input("Enter product mix demand per month (%): ")) / 100
+        self.strands = float(input("Enter number of strands: "))
+        self.area = math.pi * (self.diameter ** 2) / 4
+        self.density = billet.density
+        self.prod_rate = "Not calculated"
+        self.speed = "Not calculated"
+        self.time_gap = "Not calculated"
+        self.days = None
+        self.monthly_production = None
+
+    def production_rate(self, billet, speed, time_gap):
+        bar_length = billet.volume * self.vol_frac / (self.strands * self.wt_frac * self.area)
+        total_time = time_gap + (bar_length / speed)
+        return 3600 * self.density * billet.width * billet.thickness * billet.length / total_time
+
+    def modify(self, billet):
+        self.density = billet.density
---
+++ functions.py
@@ -0,0 +1,48 @@
+import math
+from classes import Billet, Product
+from scipy.optimize import minimize
+
+def objective(params, *variables):
+    """
+
+    :param params: (0_speed (m/s), 1_time_gap (s))
+    :param variables: (product, billet)
+    :return:
+    """
+    return -variables[0].production_rate(variables[1], params[0], params[1])
+
+
+def constraint(params, *variables):
+    """
+
+    :param params: (0_speed (m/s), 1_time_gap (s))
+    :param variables: (product, billet)
+    :return:
+    """
+    return variables[0].max_prod_rate + objective(params, *variables)
+
+
+def optimal_speed_and_time_gap(product, billet):
+    speed_bounds = (product.min_speed, product.max_speed)
+    time_gap_bounds = (product.min_time_gap, math.inf)
+    bounds = (speed_bounds, time_gap_bounds)
+    init_vals = np.array([product.max_speed, product.min_time_gap])
+    constr = {"type": "ineq", "fun": constraint, "args": (product, billet)}
+    result = minimize(objective, init_vals, method="SLSQP", bounds=bounds, constraints=constr, args=(product, billet))
+    return result
+
+
+def monthly_prod_rate(products):
+    total_days = int(input("Enter total number of working days per month: "))
+    sum = 0
+    for product in products:
+        sum += (product.demand / (24 * product.prod_rate * product.utilisation))
+    production_per_month = total_days / sum
+    for product in products:
+        product.days = product.demand * production_per_month / (24 * product.prod_rate * product.utilisation)
+        product.monthly_production = product.demand * production_per_month
+    return production_per_month, total_days
+
+
+def yearly_prod_rate(prod_per_month):
+    return 12 * prod_per_month
